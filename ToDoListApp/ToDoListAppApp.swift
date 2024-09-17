41dea89bcf515c0294654000ccb8f6d2036e960b
pratama6624 <scyoung6624@gmail.com>
2024-09-17 07:33:13 +0700

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Pratama One on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoListAppTests/ToDoListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppTests.swift
+//  ToDoListAppTests
+//
+//  Created by Pratama One on 17/09/24.
+//
+
+import Testing
+@testable import ToDoListApp
+
+struct ToDoListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
